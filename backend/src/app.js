1222a098ddc6a4e98ec6f87a73ec19e491ceb472
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-09-28 19:15:15 -0300

    Remove Passport dependencies and code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const passport = require("passport");
-const expressSession = require("express-session");
-const bodyParser = require("body-parser");
-const passportConfig = require("./auth/passport");
 
 const app = express();
 
@@ -13,18 +9,6 @@ app.set("port", process.env.PORT || 4000);
 // cors middleware
 app.use(cors());
 app.use(express.json());
-
-// passport middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  expressSession({
-    secret: "tu_secreto",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 // routes
 app.use("/api/login", require("./routes/login"));
