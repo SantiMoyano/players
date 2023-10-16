ba46aaca2d935396ff13013f5272e6db93f21197
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-10-16 17:45:55 -0300

    Add 'favouritePlayers' field to user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -11,6 +11,12 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    favouritePlayers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Player",
+      },
+    ],
   },
   {
     timestamps: true,
